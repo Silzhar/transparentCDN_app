169feb426c712c383bcc9845170ce11d59e6560c
Silzhar <luis.ruizfer@gmail.com>
2020-03-21 13:41:54 +0100

    first test in Django

--- transparentCDN_app/test_speed/models.py
+++ transparentCDN_app/test_speed/models.py
@@ -2,22 +2,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
-# import speedtest
+
 import dns.resolver
 
-# speedtester = dns.resolver.query('dnspython.org', 'MX')
-# for rdata in speedtester:
-#     print('Host ', rdata.exchange, 'preference ', rdata.preference)
 
 class TestSpeed(models.Model):
 
-    speedtester = dns.resolver.query('dnspython.org', 'MX')
+    speedtester = dns.resolver.query('transparentcdn.com', 'MX')
     for rdata in speedtester:
         print('Host ', rdata.exchange, 'preference ', rdata.preference)
+    
+    def __str__(self):
+        return self.rdata.exchange , self.rdata.preference
+    
 
-
-    # speedtester = speedtest.Speedtest()
-    # server = speedtester.get_best_server()
-
-    # print(speedtester.download())
-    # print(server)
