0e35993d8254d0d4f4502dffd9f9c64f89e0f7f3
Silzhar <luis.ruizfer@gmail.com>
2020-03-24 20:13:51 +0100

    url external / test 1

--- transparentCDN_app/test_speed/models.py
+++ transparentCDN_app/test_speed/models.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 import dns.resolver
+import speedtest
+import socket
+
 
 class TestSpeed(models.Model):
 
-    speedtester = dns.resolver.query('transparentcdn.com', 'MX')
-    dnsDates = []
-    for rdata in speedtester:
-        host = rdata.exchange
-        server = rdata.preference
-        dnsDates.append(host)
-        dnsDates.append(server)
+    urlExternal = 'www.transparentcdn.com'
+    hostAddress = socket.gethostbyname(urlExternal)
 
-    ouputDates = str(dnsDates)
-        
-    def __str__(self):
-        ouput = self.ouputDates
-     #   ouput = self.dnsDates[0] + ',' + self.dnsDates[1]
-        return ouput
-
-
-
+    speedtester = speedtest.Speedtest(source_address=hostAddress)
     
+    server = speedtester.get_best_server()
+    downloadSpeed = speedtester.download()
+    uploadSpeed = speedtester.upload()
 
+    ouputDates = []
+    ouputDates.append(server)
+    ouputDates.append(downloadSpeed)
+    ouputDates.append(uploadSpeed)
+
+    ouput = str(ouputDates)
+
+    def __str__(self):
+        return self.ouput
+      #  return self.server , self.totalQuery
